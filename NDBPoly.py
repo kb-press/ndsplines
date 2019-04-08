d2637aa44b0d32312eb99680e7995eeec8b96d88
ben <ben@sixpearls.com>
2019-04-08 10:03:50 -0700

    removing my slow implementation

--- NDBPoly.py
+++ NDBPoly.py
@@ -22,155 +22,6 @@ periodic = -1
 
 bc_map =  {clamped: "clamped", pinned: "natural", extrap: None, periodic: None}
 
-@profile
-def find_intervals(t, k, x, extrapolate=False, workspace=None):
-    """
-    Find an interval ell such that t[ell] <= x < t[ell+1].
-
-    Parameters
-    ----------
-    t : ndarray, shape=(n+k+1,) dtype=np.float_
-        knots
-    k : int
-        order of B-spline
-    x : ndarray, shape=(s,) dtype=np.float_
-        values to find the interval for
-    extrapolate : bool
-        whether to return the last or the first interval if xval
-        is out of bounds.
-
-    Returns
-    -------
-    ell : ndarray, shape=(s,) dtype=np.int_
-        Suitable interval or -1 for each value in x
-
-    Notes
-    -----
-    Similar to scipy\\interpolate\\_bspl.pyx find_Interval (inline)
-    """
-    if x.ndim != 1:
-        raise ValueError("expected 1-dimensional x")
-
-    s = x.size
-
-    do_return = False
-    if (not isinstance(workspace, np.ndarray) or 
-            (workspace.dtype != np.int_) or
-            (workspace.shape[0] < t.shape[0]) or
-            (workspace.shape[1] < s)):
-        workspace = np.empty((t.shape[0], s), dtype=np.int_)
-        do_return = True
-    
-    ell = workspace[0,:s]
-
-    # TODO: I am assuming memory is cheap and I don't get much for typing
-    # the test array as bool_ vs int_
-    test = workspace[1:t.shape[0],:s]
-
-    ell[:] = -1
-    test[:,:s] = (t[:-1,None] <= x[None,:]) & (x[None,:] < t[1:,None])
-    
-    if extrapolate:
-        test[k,:s] = test[k,:s] | (x < t[k])
-        test[-k-1,:s] = test[-k-1,:s] | (t[-k-1] <= x)
-
-    # TODO: can we pre-allocate this? or is there a better way to implement
-    # this whole function?
-    where_test = np.where(test)
-    ell[where_test[1]] = where_test[0]
-
-    if do_return:
-        return ell
-
-@profile
-def eval_bases(t, k, x, ell, nu=0, workspace=None):
-    """
-    Evaluate the k+1 non-zero spline basis functions for x
-
-    Parameters
-    ----------
-    t : ndarray, shape=(n+k+1,) dtype=np.float_
-        knots
-    k : int
-        order of B-spline.
-    x : ndarray, shape=(s,) dtype=np.float_
-        values to find the interval for
-    ell : ndarray, shape=(s,) dtype=np.int_
-        index such that t[ell] <= x < t[ell+1] for each x
-    nu : int
-        order of derivative to evaluate.
-
-    Returns
-    -------
-    u : ndarray, shape=(k+1, s) dtype=np.float_
-        the values of the non-zero spline basis functions evaluated at x
-
-    Notes
-    -----
-    similar to scipy\\interpolate\\src\\__fitpack.h _deBoor_D (inline)
-    """
-
-    if x.ndim != 1:
-        raise ValueError("expected 1-dimensional x")
-
-    s = x.size
-
-    do_return = False
-    if (not isinstance(workspace, np.ndarray) or 
-            (workspace.dtype != np.float_) or
-            (workspace.shape[0] < 2*k+3) or
-            (workspace.shape[1] < s)):
-        workspace = np.empty((2*k+3, s), dtype=np.float_)
-        do_return = True
-
-    u = workspace[:k+1,:s]
-    w = workspace[k+1:2*k+1,:s]
-    bounds = workspace[2*k+1:2*k+3,:s]
-    
-    w[0,...] = 1.0
-    for j in range(1, k-nu+1):
-
-        u[0,:] = 0
-        for n in range(1, j+1):
-            index = ell+n
-            bounds[0, :] = t[index]
-            bounds[1, :] = t[index-j]
-            neq_test = bounds[0, :] != bounds[1, :]
-            u[n, ~neq_test] = 0.0
-
-            # I'm not sure if using these views are cythonizable, but might
-            # be faster in Python+Numpy?
-            xb = bounds[0, neq_test]
-            xa = bounds[1, neq_test]
-            xx = x[neq_test]
-            tau = w[n-1, neq_test]/(xb-xa)
-            u[n-1, neq_test] += tau*(xb - xx)
-            u[n, neq_test] = tau*(xx - xa)
-            
-        # w[:j] = u[:j]
-        w[:] = u[:k].copy()
-
-    for j in range(k-nu+1, k+1):
-        u[0,:] = 0
-        for n in range(1, j+1):
-            index = ell+n
-            bounds[0, :] = t[index]
-            bounds[1, :] = t[index-j]
-            neq_test = bounds[0, :] != bounds[1, :]
-            u[n, ~neq_test] = 0.0 # __fitpack.h has h[m] = 0.0 here...
-
-            xb = bounds[0, neq_test]
-            xa = bounds[1, neq_test]
-            xx = x[neq_test]
-            tau = j*w[n-1, neq_test]/(xb-xa)
-            u[n-1, neq_test] -= tau
-            u[n, neq_test] = tau
-
-        w[:] = u[:k].copy()
-
-    if do_return:
-        return u
-    
 
 class NDBPoly(object):
     def __init__(self, knots, coeffs, orders=3, periodic=False, extrapolate=True):
@@ -244,7 +95,6 @@ class NDBPoly(object):
                 n = t.size - k - 1
                 x[i,:] = t[k] + (x[i,:] - t[k]) % (t[n] - t[k])
                 extrapolate_flag = False
-                # find_intervals(t, k, x[i,:], False, self.ell_work[i])
             else:
                 if not self.extrapolate[i,0]:
                     lt_sel = x[i,:] < t[k]
@@ -253,7 +103,6 @@ class NDBPoly(object):
                     gte_sel = t[-k-1] < x[i,:]
                     x[i,gte_sel] = t[-k-1]
                 extrapolate_flag = True
-                # find_intervals(t, k, x[i,:], True, self.ell_work[i])
 
             
 
@@ -278,6 +127,7 @@ class NDBPoly(object):
             self.uus = np.empty((self.ndim, self.cur_max_x_size, np.max(self.orders)+1,), dtype=np.float_)
             self.cc_sel = np.empty(self.c_shape_base + (self.cur_max_x_size,), dtype=np.int_)
 
+    @profile
     def __call__(self, x, nus=0):
         """
         Parameters
