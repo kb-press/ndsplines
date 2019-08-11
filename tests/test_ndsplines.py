52175656029a681da75b9124169bc18e4746aa82
ben <ben@sixpearls.com>
2019-08-11 13:49:09 -0700

    import scipy.interpolate module

--- tests/test_ndsplines.py
+++ tests/test_ndsplines.py
@@ -2,7 +2,7 @@ import pytest
 import ndsplines
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
-from scipy.interpolate import BSpline, BivariateSpline
+from scipy import interpolate
 from scipy.stats import norm
 import itertools
 
@@ -247,9 +247,10 @@ def test_file_io(ndspline):
     _make_random_spline(1, 3, ydim=1),
 ])
 def test_1d_eval(ndspline):
+    """ compare evaluation of 1-d NDSpline to scipy.interpolate.BSpline """
     bn = ndspline
     query_points = get_query_points(bn)
-    bs = BSpline(bn.knots[0], bn.coefficients, bn.degrees[0])
+    bs = interpolate.BSpline(bn.knots[0], bn.coefficients, bn.degrees[0])
     for nu in range(bn.degrees[0]+1):
         bs.extrapolate = True
         bn.extrapolate = True
@@ -277,9 +278,10 @@ def test_1d_eval(ndspline):
     _make_random_spline(2, [kx, ky], yshape=()) for kx in range(4) for ky in range(4)
 ])
 def test_2d_eval(ndspline):
+    """ compare evaluation of 2-d NDSpline to scipy.interpolate.BivariateSpline """
     bn = ndspline
     query_points = get_query_points(bn)
-    bs = BivariateSpline._from_tck(
+    bs = interpolate.BivariateSpline._from_tck(
         tuple(ndspline.knots + 
             [ndspline.coefficients.reshape(-1)] + 
             ndspline.degrees.tolist()
@@ -306,5 +308,3 @@ def test_2d_eval(ndspline):
             bs_res = bs(query_points[:,0], query_points[:,1], 0, nuy, grid=False)
 
             assert_allclose(bn_res, bs_res)
-
-
