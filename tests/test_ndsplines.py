3dfbce6ed78c4a69ac610023e682dcb2882ec98b
ben <ben@sixpearls.com>
2019-08-16 17:10:42 -0700

    adding 2d lsq test

--- tests/test_ndsplines.py
+++ tests/test_ndsplines.py
@@ -319,13 +319,13 @@ def test_2d_eval(ndspline):
 def test_1d_make_lsq(ndspline):
     N = 100
     sample_x = np.sort(get_query_points(ndspline, n=N).squeeze())
-    sample_y = ndspline(sample_x) + 0.1 * np.random.randn(N, *ndspline.yshape)
+    sample_y = ndspline(sample_x) 
     # it was non-trivial to figure out the proper parameters for
     # scipy.interpolate. It needed specific knot sequence (possibly other 
     # solutions) and sorted sample data. ndspline did not need either.
     for k in range(4):
         knots = np.r_[(0.0,)*(k+1), 0.25, 0.5, 0.75, (1.0,)*(k+1)]
-            
+
         # unweighted
         nspl = ndsplines.make_lsq_spline(sample_x, sample_y, [knots], [k])
         ispl = interpolate.make_lsq_spline(sample_x, sample_y, knots, k)
@@ -336,6 +336,53 @@ def test_1d_make_lsq(ndspline):
         nspl = ndsplines.make_lsq_spline(sample_x, sample_y, [knots], [k], w)
         ispl = interpolate.make_lsq_spline(sample_x, sample_y, knots, k, w)
         assert_allclose(nspl.coefficients.reshape(-1), ispl.c.reshape(-1))
+
+
+@pytest.mark.parametrize('ndspline', [
+    # I believe LSQBivariateSpline requires 1-D output
+    _make_random_spline(2, yshape=(1,)),
+    _make_random_spline(2, yshape=(1,)),
+    _make_random_spline(2, yshape=(1,)),
+    _make_random_spline(2, yshape=(1,)),
+])
+def test_2d_make_lsq(ndspline):
+    N = 500
+    sample_x = get_query_points(ndspline, n=N).squeeze()
+    sample_y = ndspline(sample_x)
+    for kx in range(1,4):
+        knots_x = np.r_[(0.0,)*(kx+1), 0.25, 0.5, 0.75, (1.0,)*(kx+1)]
+        for ky in range(1,4):
+            knots_y = np.r_[(0.0,)*(ky+1), 0.25, 0.5, 0.75, (1.0,)*(ky+1)]
+
+            knots = [knots_x, knots_y]
+
+            # unweighted
+            nspl = ndsplines.make_lsq_spline(sample_x, sample_y, knots, [kx, ky])
+            ispl = interpolate.LSQBivariateSpline(
+                sample_x[:, 0],
+                sample_x[:, 1], 
+                sample_y, 
+                knots_x[kx+1:-kx-1], knots_y[ky+1:-ky-1], 
+                bbox=[0.,1.,0.,1.],
+                kx=kx, ky=ky)
+            assert_allclose(ispl.get_knots()[0], nspl.knots[0])
+            assert_allclose(ispl.get_knots()[1], nspl.knots[1])
+            assert_allclose(nspl.coefficients.reshape(-1), ispl.get_coeffs().reshape(-1))
+
+            # random weights
+            w = np.random.random(N)
+            nspl = ndsplines.make_lsq_spline(sample_x, sample_y, knots, [kx, ky], w)
+            ispl = interpolate.LSQBivariateSpline(
+                sample_x[:, 0],
+                sample_x[:, 1], 
+                sample_y, 
+                knots_x[kx+1:-kx-1], knots_y[ky+1:-ky-1], 
+                w=w,
+                bbox=[0.,1.,0.,1.],
+                kx=kx, ky=ky, )
+            assert_allclose(ispl.get_knots()[0], nspl.knots[0])
+            assert_allclose(ispl.get_knots()[1], nspl.knots[1])
+            assert_allclose(nspl.coefficients.reshape(-1), ispl.get_coeffs().reshape(-1))
 
 
 
