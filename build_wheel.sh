1dff532a7e1ffcd9d5e51461b46a40752a6eeffd
Kenneth Lyons <ixjlyons@gmail.com>
2019-08-30 07:02:11 -0700

    Build manylinux wheels

---
+++ build_wheel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to build manylinux2010_x86_64 wheels.
+
+# This is intended for use with the quay.io/pypa/manylinux2010_x86_64 Docker
+# image. Azure Pipelines is set up to run this in such a container with the
+# working directory mounted at /io.
+
+set -e -x
+
+pyvers=(36 37)
+platform="manylinux2010_x86_64"
+
+for pyver in ${pyvers[@]}; do
+    pybin="/opt/python/cp${pyver}-cp${pyver}m/bin"
+    $pybin/pip install -r requirements.txt
+    $pybin/python setup.py bdist_wheel
+done
+
+for whl in dist/*.whl; do
+    auditwheel repair $whl --plat $platform -w dist
+done
