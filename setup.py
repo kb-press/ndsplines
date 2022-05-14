1c0846cc327e0182ccbf613d6c2aa0dfa8033dd4
Kenneth Lyons <ixjlyons@gmail.com>
2022-05-14 09:55:06 -0700

    Update supported Python versions in metadata

--- setup.py
+++ setup.py
@@ -63,6 +63,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     packages=["ndsplines"],
     ext_modules=extensions,
